403be319f7607801d93d639ca2c58ac0dfed10e2
giorgos <giorgos@localhost>
2009-01-19 18:30:41 +0000

    raycast check works for any texture size camera controller initialization

--- src/pano/control/ExploreState.py
+++ src/pano/control/ExploreState.py
@@ -1,6 +1,7 @@
 import logging
 
 from direct.showbase import DirectObject
+from direct.interval.IntervalGlobal import *
 
 from constants import PanoConstants
 from control.fsm import FSMState
@@ -17,9 +18,13 @@ class ExploreState(FSMState, DirectObject.DirectObject):
         self.activeNode = None
         self.cameraControl = CameraMouseControl(self.getGame())
         
+        self.talkBoxSequence = None
+        
     def enter(self):
         
         print 'entered explore state'
+        
+        self.cameraControl.initialize()
         
         #setup event handlers
         self.accept('mouse1', self.onLeftClick)
@@ -29,18 +34,37 @@ class ExploreState(FSMState, DirectObject.DirectObject):
 
         self.activeNode = self.getGame().getView().getActiveNode()
         
-        self.getGame().getView().panoRenderer.drawDebugHotspots(True)        
+        self.getGame().getView().panoRenderer.drawDebugHotspots(
+                self.getGame().getConfig().getBool(PanoConstants.CVAR_DEBUG_HOTSPOTS, False))
+        
+        self.talkBoxSequence = Sequence(
+                                        Func(self.getGame().getI18n().setLanguage, "en"),
+                                        Func(self.getGame().getView().getTalkBox().showText, "node1.door.desc"),
+                                        Wait(3.0),
+                                        Func(self.getGame().getI18n().setLanguage, "gr"),
+                                        Func(self.getGame().getView().getTalkBox().showText, "node1.door.desc"),
+                                        Wait(3.0))
+        self.talkBoxSequence.loop()        
     
     def onLeftClick(self):
         # returns the face code and image space coordinates of the hit point    
-        face, x, y = self.getGame().getView().raycastNodeAtMouse()
-        x *= 1024
-        y *= 1024
+        face, x, y = self.getGame().getView().raycastNodeAtMouse()        
+        dim = self.getGame().getView().panoRenderer.getFaceTextureDimensions(face)
+        x *= dim[0]
+        y *= dim[1]
+        
         for h in self.activeNode.getHotspots():
             if h.getFace() == face and x >= h.getXo() and x <= h.getXe() and y >= h.getYo() and y <= h.getYe():
                 if self.log.isEnabledFor(logging.DEBUG):                    
-                    self.log.debug('Clicked on hotspot %s, (%s)', h.getName(), h.getDescription()) 
-                self.getGame().actions().execute(h.getAction(), h.getActionArgs())
+                    self.log.debug('Clicked on hotspot %s, (%s)', h.getName(), h.getDescription())
+                
+                self.talkBoxSequence = Sequence(Func(self.getGame().getView().getTalkBox().showText, h.getDescription()),
+                                        Wait(3.0),
+                                        Func(self.getGame().getView().getTalkBox().hide))
+                self.talkBoxSequence.start()                 
+                
+                if h.getAction() is not None:
+                    self.getGame().actions().execute(h.getAction(), h.getActionArgs())
                 
 #        print 'face culling test:\n'
 #        print 'testing isFaceInFrustum from front: ', self.getGame().getView().panoRenderer.isFaceInFrustum(PanoConstants.CBM_FRONT_FACE)
@@ -55,6 +79,9 @@ class ExploreState(FSMState, DirectObject.DirectObject):
         self.ignoreAll()
         
         self.cameraControl.disable()
+        
+        if self.talkBoxSequence is not None:
+            self.talkBoxSequence.finish()
     
     def update(self, millis):
         self.cameraControl.update(millis)
