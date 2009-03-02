ba7bfe98f367b930a7b5dc8ed22715bed598b3b7
giorgos <giorgos@localhost>
2009-03-02 15:55:07 +0000

    added inventory property and methods to convert between screen and aspect2d spaces

--- src/pano/view/GameView.py
+++ src/pano/view/GameView.py
@@ -7,6 +7,7 @@ from MousePointerDisplay import MousePointerDisplay
 from NodeRaycaster import NodeRaycaster
 from model.Node import Node
 from TalkBox import TalkBox
+from inventoryView import InventoryView
 
 class GameView:    
     def __init__(self, gameRef = None, title = ''):
@@ -19,15 +20,18 @@ class GameView:
         self.panoRenderer = NodeRenderer(self.game.resources)
         self.raycaster = NodeRaycaster(self.panoRenderer)        
         self.mousePointer = MousePointerDisplay(gameRef)
+        self.inventory = InventoryView(gameRef)
         self.__talkBox = TalkBox(gameRef)         
         self.activeNode = None   
         
     def initialize(self):
+        self.windowProperties = base.win.getProperties()
         base.setFrameRateMeter(self.game.getConfig().getBool(PanoConstants.CVAR_DEBUG_FPS))
         self.panoRenderer.initialize()
         self.mousePointer.initialize()
         self.raycaster.initialize()
         self.__talkBox.initialize()
+        self.inventory.initialize(self.game.getInventory())
                      
 
     def getTalkBox(self):
@@ -36,6 +40,8 @@ class GameView:
     def setTalkBox(self, value):
         self.__talkBox = value
 
+    def getInventoryView(self):
+        return self.inventory
         
     def displayNode(self, node):
         self.activeNode = node
@@ -43,8 +49,11 @@ class GameView:
         
     def raycastNodeAtMouse(self):
         #This gives up the screen coordinates of the mouse
-        mpos = base.mouseWatcherNode.getMouse()
-        return self.raycaster.raycastMouse(mpos.getX(), mpos.getY())
+        if base.mouseWatcherNode.hasMouse():
+            mpos = base.mouseWatcherNode.getMouse()
+            return self.raycaster.raycastMouse(mpos.getX(), mpos.getY())
+        else:
+            return None
         
     def getActiveNode(self):
         return self.activeNode
@@ -52,8 +61,11 @@ class GameView:
     """
     Returns a reference to a MousePointer object that controls the mouse pointer.
     """
-    def mousePointer(self):
+    def getMousePointer(self):
         return self.mousePointer
+    
+    def getWindowProperties(self):
+        return self.windowProperties
     
     def setWindowProperties(self, props = {}):
         wp = WindowProperties()
@@ -97,6 +109,37 @@ class GameView:
     def update(self, millis):
         self.panoRenderer.render(millis)
         self.talkBox.update(millis)
+        self.inventory.update(millis)
+        
+    def convertScreenToAspectCoords(self, pointsList):
+        wp = self.getWindowProperties()
+        # the two factors below will convert x and y components into the [0.0...2*base.a2dRight] and [0...2*base.a2dTop] 
+        # ranges respectively
+        aspectXScale = (1.0 / wp.getXSize()) * (base.a2dRight - base.a2dLeft)
+        aspectYScale = (1.0 / wp.getYSize()) * (base.a2dTop - base.a2dBottom)
+        retList = []
+        for p in pointsList: 
+            # subtract maximum values to scale into [base.a2dLeft...base.a2dRight] and [base.a2dBottom...base.a2dTop]
+            # and reverse y direction too            
+            x = p[0]*aspectXScale - base.a2dRight
+            y = -1.0*(p[1]*aspectYScale - base.a2dTop)
+            retList.append((x, y))
+        return retList
+    
+    def convertAspectToScreenCoords(self, pointsList):
+        """
+        Converts a list of points defined in the aspect2d coordinate space, to the screen coordinate space.
+        Returns a list of transformed points.
+        """
+        retList = []
+        for p in pointsList:
+            # this converts into the [-1.0...1.0] range
+            rp = render2d.getRelativePoint(aspect2d, Point3(p[0], 0, p[1]))
+            # scale to viewport and add, y also needs reversing
+            x = ((rp[0] + 1.0) / 2.0) * wp.getXSize()
+            y = -1.0*((rp[0] + 1.0) / 2.0) * wp.getYSize()
+            retList.append((x, y))
+        return retList
 
     talkBox = property(getTalkBox, setTalkBox, None, "TalkBox's Docstring")
         
