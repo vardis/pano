e716e830c8fa87ee0964710772b5834b43ae7e39
Vardis <Vardis@localhost>
2009-04-28 17:23:37 +0300

    initial commit

---
+++ src/pano/util/PandaUtil.py
@@ -0,0 +1,54 @@
+'''
+    Copyright (c) 2008 Georgios Giannoudovardis, <vardis.g@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+'''
+
+class PandaUtil(object):
+    '''
+    Contains static utility methods for dealing with Panda3D related objects.
+    '''
+
+    def getFontPixelPerfectScale(font):
+        '''
+        Returns the font scale that will render this font with a one-to-one correspondence between texels
+        and pixels.
+        It assumes that the font will be parented to aspect2d and that the main window can be accessed by base.win . 
+        '''
+        winHeight = base.win.getYSize()
+        ppu = font.getPixelsPerUnit()        
+        return (2.0 * ppu) / winHeight
+    
+    def unitsToPixelsY(units, useAspect = True):    
+        '''
+        Translates the given world units to pixels along the vertical window dimension.
+        If the parameter useAspect is True then the aspect ratio of the window will be considered.
+        '''
+        winHeight = float(base.win.getYSize())
+        h = 2.0
+        if useAspect:
+            h = base.a2dTop - base.a2dBottom
+        return winHeight * units / h
+    
+    getFontPixelPerfectScale = staticmethod(getFontPixelPerfectScale)
+    unitsToPixelsY = staticmethod(unitsToPixelsY)
+    
+
+        
